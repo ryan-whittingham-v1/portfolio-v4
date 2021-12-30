bf9a142e8df180d45e7043f761a45cbfbf637a30
Ryan Whittingham <whittingham777@gmail.com>
2021-12-29 23:45:41 -0800

    cloudinary integration

--- components/Bio.js
+++ components/Bio.js
@@ -1,11 +1,14 @@
 import styles from '../styles/bio.module.css';
+import Pic from '../components/Pic';
 
 export default function Bio(props) {
-  console.log(props.bio.fields.body);
   return (
     <div className={styles.bio}>
       <h1>{props.bio.fields.title}</h1>
       <p>{props.bio.fields.content}</p>
+      <Pic
+        publicId={'portfolio/69dd4c4b-047e-4965-b289-e53849985bc1_2_epoybd'}
+      />
     </div>
   );
 }
---
+++ components/Pic.js
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import { buildUrl } from 'cloudinary-build-url';
+import styles from '../styles/pic.module.css';
+
+export default function Pic(props) {
+  const url = buildUrl(props.publicId, {
+    cloud: {
+      cloudName: 'whittingham-io',
+    },
+    transformations: {
+      effect: {
+        effect: 'blur:1000',
+        quality: 1,
+      },
+    },
+  });
+  return (
+    <div className={styles.container}>
+      <Image src={url} alt="Galaxy" layout="fill" objectFit="contain" />
+    </div>
+  );
+}
