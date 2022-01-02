9e5d6e25c467509e71ebfb3848bdf27f77704d66
Ryan Whittingham <whittingham777@gmail.com>
2022-01-02 11:40:04 -0800

    contentful staticPaths

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,7 +1,20 @@
-import { fetchEntry } from '../../lib/contentful';
+import { fetchEntries, fetchEntry } from '../../lib/contentful';
 
 export default function Project({ entry }) {
   return <div>{entry.fields.title}</div>;
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const entries = await fetchEntries();
+  // Get the paths we want to pre-render based on posts
+  const paths = entries.map((entry) => ({
+    params: { id: entry.sys.id },
+  }));
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
 }
 
 // This also gets called at build time
