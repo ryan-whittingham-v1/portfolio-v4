c976267b5a44f8ae50bb4e795d8d2cd4249e5b02
Ryan Whittingham <whittingham777@gmail.com>
2022-01-30 17:34:51 -0800

    add folder emoji

--- components/Projects.js
+++ components/Projects.js
@@ -5,7 +5,7 @@ export default function Projects(props) {
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
-        <h1>PROJECTS</h1>
+        <h1>PROJECTS 📁</h1>
       </div>
       <div className={styles.projects}>
         {props.projects.map((p) => {
